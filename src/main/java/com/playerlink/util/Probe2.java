c4851d059cbf7a89963e4fb807a53a724e86cd88
femthyfox <thefemmyfox@gmail.com>
2026-05-31 15:40:31 +0100

    probe fix 1.0.8

--- src/main/java/com/playerlink/util/Probe2.java
+++ src/main/java/com/playerlink/util/Probe2.java
@@ -3,11 +3,8 @@ package com.playerlink.util;
 public final class Probe2 {
     private Probe2() {}
     public static void run() {
-        // Each line force-loads a class. Build fails with a CLEAR error
-        // if the class path is wrong, so we can fix it before writing mixins.
         Class<?> a = com.simibubi.create.content.redstone.link.controller.LinkedControllerServerHandler.class;
         Class<?> b = com.simibubi.create.content.redstone.link.controller.LinkedControllerClientHandler.class;
-        Class<?> c = com.simibubi.create.content.redstone.link.controller.LinkedControllerBlockItem.class;
-        System.out.println("[PlayerLink Probe2] OK: " + a + " " + b + " " + c);
+        System.out.println("[PlayerLink Probe2] OK: " + a + " " + b);
     }
 }
